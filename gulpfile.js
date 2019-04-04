83a28332db49113789cacc32fefd593ce31a2f20
lukasso <lukasz.koladko@gmail.com>
2019-04-04 17:45:56 +0200

    PR190404-001

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var browserSync = require('browser-sync').create();
 gulp.task('browserSync', function () {
   browserSync.init({
     server: {
-    baseDir: './Mountain'
+    baseDir: './Ocean'
     },
   })
 });
 gulp.task('watch', ['browserSync'], () => {
-    gulp.watch('Mountain/*.html', browserSync.reload);
-    gulp.watch('Mountain/*.css', browserSync.reload);
+    gulp.watch('Ocean/*.html', browserSync.reload);
+    gulp.watch('Ocean/*.css', browserSync.reload);
   });
